4646d02a9f045c9547d7b1c189920ecbe677931b
MblWa <MblWa@users.noreply.github.com>
2021-02-05 00:07:19 +0300

    Исправлен порядок функций

--- js/main.js
+++ js/main.js
@@ -46,6 +46,59 @@ const PHOTOS_OF_HOUSING = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
+//Генерация целых чисел из диапазона положительных чисел включительно.
+//Т.к. в ТЗ не прописано, поступают ли на вход целочисленные значения границ диапазона,
+//то будем округлять их до целого значения, даже если придут с плавающей точкой.
+
+const getIntFromRange = (min, max) => {
+  let randomNumber = undefined;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (max < min) {
+    let buffer = max;
+    max = min;
+    min = buffer;
+  }
+
+  if (min === max) {
+    randomNumber = min;
+  }
+
+  if (min >= 0 && max >= 0) {
+    randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  return randomNumber;
+}
+
+//Реализация функции под Кексобукинг с числами с плавающей точкой.
+//Т.к. Math.random оперирует числами в диапазоне [0, 1), исключая единицу,
+//придётся включать данную границу за счет округления и увеличения на
+//машинный эпсилон, что позволяет включить единицу.
+
+const getFloatFromRange = (min, max, digits) => {
+  let randomNumber = undefined;
+
+  digits = (digits > PRECISION) ? PRECISION : digits;
+
+  if (max < min) {
+    let buffer = max;
+    max = min;
+    min = buffer;
+  }
+
+  if (min === max) {
+    randomNumber = min;
+  }
+
+  if (min >= 0 && max >= 0) {
+    randomNumber = Math.round((Math.random() * (max - min) + min + Number.EPSILON) * Math.pow(10, digits)) / Math.pow(10, digits);
+  }
+
+  return randomNumber;
+}
+
 //Случайное значение из массива
 const getRandomStringFromMap = (array) => {
   return array[getIntFromRange(0, array.length - 1)];
@@ -106,59 +159,6 @@ const createNearbyAdvert = () => {
     },
   };
 };
-
-//Генерация целых чисел из диапазона положительных чисел включительно.
-//Т.к. в ТЗ не прописано, поступают ли на вход целочисленные значения границ диапазона,
-//то будем округлять их до целого значения, даже если придут с плавающей точкой.
-
-const getIntFromRange = (min, max) => {
-  let randomNumber = undefined;
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  if (max < min) {
-    let buffer = max;
-    max = min;
-    min = buffer;
-  }
-
-  if (min === max) {
-    randomNumber = min;
-  }
-
-  if (min >= 0 && max >= 0) {
-    randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  return randomNumber;
-}
-
-//Реализация функции под Кексобукинг с числами с плавающей точкой.
-//Т.к. Math.random оперирует числами в диапазоне [0, 1), исключая единицу,
-//придётся включать данную границу за счет округления и увеличения на
-//машинный эпсилон, что позволяет включить единицу.
-
-const getFloatFromRange = (min, max, digits) => {
-  let randomNumber = undefined;
-
-  digits = (digits > PRECISION) ? PRECISION : digits;
-
-  if (max < min) {
-    let buffer = max;
-    max = min;
-    min = buffer;
-  }
-
-  if (min === max) {
-    randomNumber = min;
-  }
-
-  if (min >= 0 && max >= 0) {
-    randomNumber = Math.round((Math.random() * (max - min) + min + Number.EPSILON) * Math.pow(10, digits)) / Math.pow(10, digits);
-  }
-
-  return randomNumber;
-}
 
 const getAdvertsNearBy = (amount) => {
   return new Array(amount).fill(null).map(() => createNearbyAdvert());
