59edff74492a9ef04fd17a6ad1159ccb61c37062
MblWa <MblWa@users.noreply.github.com>
2021-02-17 09:11:23 +0300

    Перенесены ограничения стандартных границ инпута цены в разметку html

--- js/form.js
+++ js/form.js
@@ -4,16 +4,12 @@ const housingPrice = form.querySelector('#price');
 const checkinSelect = form.querySelector('#timein');
 const checkoutSelect = form.querySelector('#timeout');
 
-const MAXIMUM_HOUSING_PRICE = 1000000;
 const MINIMUM_HOUSING_PRICE = {
   'palace': 10000,
   'flat': 1000,
   'house': 5000,
   'bungalow': 0,
 };
-
-housingPrice.min = 0;
-housingPrice.max = MAXIMUM_HOUSING_PRICE;
 
 housingType.addEventListener('change', () => {
   housingPrice.placeholder = MINIMUM_HOUSING_PRICE[housingType.value];
