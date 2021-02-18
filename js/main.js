02a37270f5cc82e9e82c4e3024d74db43d5b692b
MblWa <MblWa@users.noreply.github.com>
2021-02-18 20:27:20 +0300

    Убрана отрисовка сэмпла объявления на карте

--- js/main.js
+++ js/main.js
@@ -7,4 +7,3 @@ const NEARBY_ADVERTS_QTY = 10;
 const adverts = getAdvertsNearBy(NEARBY_ADVERTS_QTY);
 
 const map = document.querySelector('#map-canvas');
-map.appendChild(generateCard(adverts[0]));
