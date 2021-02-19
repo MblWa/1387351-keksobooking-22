f09d11c26a46bc9a809d889e62c785c9f3e5d289
MblWa <MblWa@users.noreply.github.com>
2021-02-19 22:51:43 +0300

    Замена имени переменной для работы с Лифлетом.

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ import { selectForm, disableForm, enableForm, updateAddress } from './form.js';
 import { getAdvertsNearBy } from './data.js';
 import { generateCard } from './generate-template.js';
 
-const L = window.L;
+const LEAFLET_MAP = window.L;
 //Константа для задания количества схожих объявлений при их генерации
 const NEARBY_ADVERTS_QTY = 10;
 
@@ -20,7 +20,7 @@ const filterForm = selectForm('.map__filters');
 disableForm(adForm, 'ad-form--disabled');
 disableForm(filterForm, 'map__filters--disabled');
 
-const map = L.map(mapLayer)
+const map = LEAFLET_MAP.map(mapLayer)
   .on('load', () => {
     enableForm(adForm, 'ad-form--disabled');
     enableForm(filterForm, 'map__filters--disabled');
@@ -28,20 +28,20 @@ const map = L.map(mapLayer)
   })
   .setView(TOKYO_CITY_CENTER_COORD, ZOOM);
 
-L.tileLayer(
+LEAFLET_MAP.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ).addTo(map);
 
-const mainPinIcon = L.icon({
+const mainPinIcon = LEAFLET_MAP.icon({
   iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
 
-const mainMarker = L.marker(
+const mainMarker = LEAFLET_MAP.marker(
   TOKYO_CITY_CENTER_COORD,
   {
     draggable: true,
@@ -57,12 +57,12 @@ mainMarker.on('moveend', (evt) => {
 const adverts = getAdvertsNearBy(NEARBY_ADVERTS_QTY);
 
 adverts.forEach((advert) => {
-  const adPinIcon = L.icon({
+  const adPinIcon = LEAFLET_MAP.icon({
     iconUrl: './img/pin.svg',
     iconSize: [26, 26],
     iconAnchor: [13, 26],
   });
-  const adMarker = L.marker(
+  const adMarker = LEAFLET_MAP.marker(
     {
       lat: advert.location.x,
       lng: advert.location.y,
